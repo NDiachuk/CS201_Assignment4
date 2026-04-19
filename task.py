79703fd1409052d1ecfc06526693fb5a4f07964a
Nazar <nazar.diachu.25@kse.org.ua>
2026-04-19 20:42:54 +0300

    створив графік

--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 data = pd.read_csv('random_walk.csv')
 distance=round((data['x']**2+data['y']**2)**0.5,1)
 data['distance']=distance
@@ -10,4 +11,11 @@ print("average distance",average)
 filtered_walk=data[data['distance']>average]
 print("avarage <",filtered_walk)
 filtered_walk.to_json('filtered_walk.json')
+plt.scatter(data['x'],data['y'],color='green')
+plt.title('random walk')
+plt.xlabel('x',fontsize=20)
+plt.ylabel('y',fontsize=20)
+plt.legend(['trajectory'] ,fontsize=10,loc='lower right')
+plt.scatter([0,-2], [0,-8], color='blue', s=80)
+plt.show()
 
