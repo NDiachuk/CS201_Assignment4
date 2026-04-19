24bc35677ab3c17322ba87f336159f939f1133d0
Nazar <nazar.diachu.25@kse.org.ua>
2026-04-19 19:46:37 +0300

    вивів статистичні дані і таблицю, де відстані більші середньої

--- task.py
+++ task.py
@@ -3,3 +3,7 @@ data = pd.read_csv('random_walk.csv')
 distance=round((data['x']**2+data['y']**2)**0.5,1)
 data['distance']=distance
 print(data)
+print("max distance",max(data['distance']))
+average=round(sum(data['distance'])/len(data['distance']),3)
+print("average distance",average)
+print("avarage <",data[(data['distance']>average)])
